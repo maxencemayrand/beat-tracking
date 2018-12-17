304f07cd75d9e6a2f3be74d839773e5b305b6d74
Maxence Mayrand <35958639+maxencemayrand@users.noreply.github.com>
2018-12-17 13:03:57 +0000

    constants

---
+++ constants.py
@@ -0,0 +1,5 @@
+sr = 16000    # sampling rate
+hl = 512      # hop length
+nb = 229      # number of frequency bins in the spectrograms
+fm = 30.0     # lowest frequency in spectrograms
+delta = 0.05  # maximum distance between an onset and a beat
